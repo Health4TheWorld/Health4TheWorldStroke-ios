b7266e1d5ed51b331e7503787dacd662d090f6c9
MacOS ElCapitan <Premnath@MacOS-ElCapitans-Mac.local>
2017-04-23 13:05:24 -0700

    appended Arms section files

--- H4TWStrokeHope/ArmsAndHandsViewController.m
+++ H4TWStrokeHope/ArmsAndHandsViewController.m
@@ -8,6 +8,10 @@
 
 #import "ArmsAndHandsViewController.h"
 #import "Constants.h"
+#import "HomeButton.h"
+#import "HandStretchViewController.h"
+#import "ShoulderStretchViewController.h"
+#import "ArmStretchViewController.h"
 
 
 @interface ArmsAndHandsViewController ()
@@ -30,6 +34,58 @@
     backBtn.frame = CGRectMake(0, 0, 15, 25);
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithCustomView:backBtn];
     self.navigationItem.leftBarButtonItem = backButton;
+    
+    [self setUpView];
+}
+
+
+- (void)setUpView {
+    static int SPACE_BETWEEN_CELLS = 10;
+    float cellWidth = ([UIScreen mainScreen].bounds.size.width / 2) - (SPACE_BETWEEN_CELLS) - (SPACE_BETWEEN_CELLS / 2);
+    float cellHeight = cellWidth;
+    
+    float startingY = SPACE_BETWEEN_CELLS;
+    
+    HomeButton *handStretchButton = [[HomeButton alloc] initWithText:@"Hand Stretch" withFrame:CGRectMake(SPACE_BETWEEN_CELLS, startingY, cellWidth, cellHeight)];
+    [handStretchButton addImageCentered:[UIImage imageNamed:STRETCHING_HAND_STRETCH_1]];
+    [handStretchButton addTarget:self action:@selector(handStretchPressed) forControlEvents:UIControlEventTouchUpInside];
+    
+    HomeButton *shoulderStretchButton = [[HomeButton alloc] initWithText:@"Shoulder Stretch" withFrame:CGRectMake((self.view.frame.size.width / 2) + (SPACE_BETWEEN_CELLS / 2), startingY, cellWidth, cellHeight)];
+    [shoulderStretchButton addImageCentered:[UIImage imageNamed:STRETCHING_SHOULDER_STRETCH_1]];
+    [shoulderStretchButton addTarget:self action:@selector(shoulderStretchPressed) forControlEvents:UIControlEventTouchUpInside];
+    
+    startingY += cellWidth;
+    startingY += SPACE_BETWEEN_CELLS;
+    cellWidth = ([UIScreen mainScreen].bounds.size.width) - (SPACE_BETWEEN_CELLS * 2);
+    
+    HomeButton *armStretchButton = [[HomeButton alloc] initWithText:@"Arm Stretch" withFrame:CGRectMake(SPACE_BETWEEN_CELLS, startingY, cellWidth, cellHeight)];
+    [armStretchButton addImageCentered:[UIImage imageNamed:STRETCHING_ARM_STRETCH_1]];
+    [armStretchButton addTarget:self action:@selector(armStretchPressed) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    [self.contentView addSubview: handStretchButton];
+    [self.contentView addSubview: shoulderStretchButton];
+    [self.contentView addSubview: armStretchButton];
+    
+    
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, startingY);
+}
+
+/* Action listeners for buttons */
+
+- (void)handStretchPressed {
+    HandStretchViewController *vc = [[HandStretchViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+- (void)shoulderStretchPressed {
+    ShoulderStretchViewController *vc = [[ShoulderStretchViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+- (void)armStretchPressed {
+    ArmStretchViewController *vc = [[ArmStretchViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 
