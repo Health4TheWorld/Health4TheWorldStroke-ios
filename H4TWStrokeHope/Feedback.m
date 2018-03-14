fc9993eaa8536ed428e143a9cb06af5bbff01ab4
Maitri <maitri.b.mehta@gmail.com>
2018-03-14 12:48:03 -0700

    Feedback

---
+++ H4TWStrokeHope/Feedback.m
@@ -0,0 +1,38 @@
+//
+//  Feedback.m
+//  H4TWStrokeHope
+//
+//  Created by Maitri Mehta on 3/14/18.
+//  Copyright © 2018 Rachel. All rights reserved.
+//
+
+#import "Feedback.h"
+
+@implementation Feedback
+
+static NSString *const TableName = @"Feedback";
+static NSString *const HashKeyAttribute = @"DeviceId";
+
+// Helper Tasks for the table
++ (AWSTask *)describeTable {
+    AWSDynamoDB *dynamoDB = [AWSDynamoDB defaultDynamoDB];
+    
+    // See if the test table exists.
+    AWSDynamoDBDescribeTableInput *describeTableInput = [AWSDynamoDBDescribeTableInput new];
+    describeTableInput.tableName = TableName;
+    return [dynamoDB describeTable:describeTableInput];
+}
+
+// Confirm to AWS DynamoDb oBject Model protocol
+
++ (NSString *)dynamoDBTableName {
+    return TableName;
+}
+
++ (NSString *)hashKeyAttribute {
+    return HashKeyAttribute;
+}
+
+
+
+@end
